259e358cf3d0b031ead6c7bf26dc95d32e08f29e
Luiz Carlos Cunha <luiz.cunha.3@gmail.com>
2023-04-21 10:00:05 -0300

    add random color for testing

--- luiz-sdk/Classes/UIView+Extension.swift
+++ luiz-sdk/Classes/UIView+Extension.swift
@@ -5,4 +5,24 @@ public extension UIView {
     var ds: DSConstraint {
         return DSConstraint(view: self)
     }
+    func makeDebug() {
+        self.backgroundColor = .randomTransparent()
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func randomTransparent() -> UIColor {
+        return UIColor(
+           red:   .random(),
+           green: .random(),
+           blue:  .random(),
+           alpha: 0.25
+        )
+    }
 }
